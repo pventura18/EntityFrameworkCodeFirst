2650c79b15a6d181673776daa10adf567d5859e2
Pol Ventura <polventurapont@gmail.com>
2024-03-05 22:10:58 +0100

    Added wnd price of orders window

---
+++ EntityFrameworkCodeFirst/Windows/WNDPriceOfOrders.xaml.cs
@@ -0,0 +1,30 @@
+﻿using EntityFrameworkCodeFirst.DAO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EntityFrameworkCodeFirst.Windows
+{
+    /// <summary>
+    /// Lógica de interacción para WNDPriceOfOrders.xaml
+    /// </summary>
+    public partial class WNDPriceOfOrders : Window
+    {
+        public WNDPriceOfOrders(IDAO manager)
+        {
+            InitializeComponent();
+            lvOrderPrice.ItemsSource = (IEnumerable)manager.GetPriceOfOrders();
+        }
+    }
+}
